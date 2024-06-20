da60f63632ac61d3f7f451c01aa38507844acf1d
Prashantjha14 <129178920+Prashantjha14@users.noreply.github.com>
2024-06-20 13:19:58 +0530

    remove the default caching from get routes

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,17 +2,19 @@ import { connectToDB } from "@/utils/database";
 import Prompt from "@/models/prompt";
 
 export const GET = async (req) => {
-    try {
-        await connectToDB();
+  const headers = req.headers;
 
-        const prompts = await Prompt.find({}).populate('creator');
+  try {
+    await connectToDB();
 
-        return new Response(JSON.stringify(prompts), {
-            status: 200,
-        })
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", {
-            status: 500,
-        })
-    }
-}
+    const prompts = await Prompt.find({}).populate("creator");
+
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", {
+      status: 500,
+    });
+  }
+};
